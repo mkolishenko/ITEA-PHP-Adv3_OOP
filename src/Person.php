2b81f0b0e0e219c63eff6767698145dc23cb35c1
Maksym <mkolishenko@gmail.com>
2018-11-29 20:26:45 +0200

    Getters and Setters

--- src/Person.php
+++ src/Person.php
@@ -8,9 +8,14 @@
 
 class Person
 {
+
+    const MAX_POSSIBLE_AGE = 50;
+
     private $username;
     private $email;
     private $age;
+
+
 
     public function __construct($username, $email)
     {
@@ -23,4 +28,26 @@ class Person
     {
         return $this->username;
     }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function setAge($age)
+    {
+        if ($age > self::MAX_POSSIBLE_AGE)
+        {
+            echo "Cannot create person with provided age".$age;
+            return;
+        }
+        $this->age = $age;
+    }
+
+
 }
